38596225fa839b39fa02643eca5393bef9b8f6f2
Andrew McWatters <andrew@andrewmcwatters.com>
2022-04-09 10:51:11 -0700

    Detailed Instructions
    https://wordpress.org/support/article/how-to-install-wordpress/#detailed-instructions

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,7 @@ sudo apt-get -y install \
   php \
   mysql-server \
   apache2
+
+# https://wordpress.org/support/article/how-to-install-wordpress/#detailed-instructions
+wget https://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz
