55ee844847dd591e0f26280f1b6200fcc43582b8
Andrew McWatters <andrew@andrewmcwatters.com>
2022-04-09 11:03:55 -0700

    Change mysql root password
    https://dev.mysql.com/doc/refman/8.0/en/resetting-permissions.html

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,3 +11,10 @@ sudo apt-get -y install \
 # https://wordpress.org/support/article/how-to-install-wordpress/#detailed-instructions
 wget https://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
+
+# https://dev.mysql.com/doc/refman/8.0/en/resetting-permissions.html
+# https://dev.mysql.com/doc/refman/8.0/en/alter-user.html
+mysql --user=root --execute="ALTER USER 'root'@'localhost'
+  IDENTIFIED WITH mysql_native_password
+             BY '';
+"
